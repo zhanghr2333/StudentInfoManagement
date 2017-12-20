d5e97ddecf4c10ded85661dda2b80981017e70d9
OhnkytaBlabdey <1263189143@qq.com>
2017-12-20 19:00:59 +0800

    2

--- StudentInfoManagement/module.c
+++ StudentInfoManagement/module.c
@@ -1,15 +1,26 @@
 //module
-#include "module.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+#include <malloc.h>
+
+#include "module.h"
+#define pr printf
+
 void print_stu_node(Stu *pstu)
 {
-    printf("print_stu_node");
-    ;
+    printf("\nprint_stu_node:\n");
+    print_stu_data(&pstu->data);
+    pr("\nprint_stu_node ended\n");
 }
 
 void print_stu_data(StuData *pdata)
 {
-    ;
+    pr("\n\tprint_stu_data:\n");
+    pr("\t\tÑ§ºÅ£º%d",pdata->seq);
+    pr("\n\tprint_stu_data ended\n");
 }
 
 
