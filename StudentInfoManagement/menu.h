dbd73679471c9370367d1069c9954719b61eaa9f
OhnkytaBlabdey <1263189143@qq.com>
2017-12-21 12:02:33 +0800

    2- fix

--- StudentInfoManagement/menu.h
+++ StudentInfoManagement/menu.h
@@ -7,5 +7,5 @@ int search_by_seq(Stu *phead, Stu *prear);
 
 int search_by_name(Stu *phead, Stu *prear);
 
-int edit_stu_infoStu(Stu *phead, Stu *prear);
+int edit_stu_info(Stu *phead, Stu *prear);
 
